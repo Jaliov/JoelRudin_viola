5103585c68b7bdae8f21d3cc0277373e40552fc4
Joel Rudin <jarud55@gmail.com>
2023-04-23 09:48:25 -0400

    fancybox viewer, copy edits

--- nav.js
+++ nav.js
@@ -2,21 +2,21 @@
 document.querySelector(".nav").innerHTML = `
 <nav class="navbar navbar-expand-lg navbar-light">
 <div class="container-fluid">
-<a class="navbar-brand" href="index.html">Joel Rudin</a>
+<a class="navbar-brand" href="index.html"  id="home" >Joel Rudin</a>
 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
   <span class="navbar-toggler-icon"></span>
 </button>
 <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
   <div class="navbar-nav">
-    <a class="nav-item nav-link active" aria-current="page" href="index.html" id = "home">Home</a>
-    <a class="nav-item nav-link" href="Joelbio.html">About Joel</a>
-    <a class="nav-item nav-link" href="violaclips.html">Viola Tracks</a>
-    <a class="nav-item nav-link" href="teaching.html">Teaching/Contact</a>
+    <a class="nav-item nav-link active" aria-current="page" href="index.html" id="home" >Home</a>
+    <a class="nav-item nav-link" href="Joelbio.html"  id="home">About Joel</a>
+    <a class="nav-item nav-link" href="violaclips.html"  id="home">Viola Tracks</a>
+    <a class="nav-item nav-link" href="teaching.html"  id="home">Teaching/Contact</a>
     <!-- <a class="nav-item nav-link disabled" href="#">Disabled</a> -->
   </div>
 </div>
 </div>
-</nav>
+
 </nav>
 `
 
